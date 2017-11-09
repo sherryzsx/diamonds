6b92ad928a3b19de7404fd3e942367e4cf283e90
sherryzsx <shuxiaoz@usc.edu>
2017-11-08 19:42:57 -0800

    This is the diamond lab

---
+++ diamondsVis/app.R
@@ -0,0 +1,41 @@
+library(shiny)
+library(ggplot2)
+
+#user interface
+
+ui <- fluidPage(
+  titlePanel("Diamonds Data",windowTitle = "Diamonds"),
+  sidebarLayout(
+    sidebarPanel(
+      helpText("This app is to visualize diamonds dataset"),
+      textInput(inputId = "title",label = "Chart Title", value = ""),
+      selectInput(inputId = "pos",
+                  label = "Position",
+                  choices = list("stack","dodge"),
+                  selected = "stack")
+    ),
+    mainPanel(
+      plotOutput(outputId = "plot")
+    )
+  )
+)
+
+# Server
+
+server <- function(input, output){
+  output$plot = 
+    renderPlot(ggplot(diamonds, aes(x=cut,fill=clarity))+
+                 geom_bar(position = input$pos)+
+                 ggtitle(input$title)
+               )
+  
+}
+
+# run the app
+
+shinyApp(ui,server)
+
+
+
+
+
